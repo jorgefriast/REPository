bcf698847a10016d60155cb84beca1f6f37605e0
SenecalJF <jeanjeansenecal@gmail.com>
2024-03-30 19:29:24 +0100

    style : removed unused import

--- desktop/src/com/introduction/rowing/DesktopLauncher.java
+++ desktop/src/com/introduction/rowing/DesktopLauncher.java
@@ -2,7 +2,6 @@ package com.introduction.rowing;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.introduction.rowing.MyRowing;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
