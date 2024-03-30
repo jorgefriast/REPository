4391402a1a5b8d17ea3a550cdaf7257f0cf310fc
SenecalJF <jeanjeansenecal@gmail.com>
2024-03-30 13:16:46 +0100

    feat: init of gradle and libgdx

---
+++ desktop/src/com/introduction/rowing/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.introduction.rowing;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.introduction.rowing.MyRowing;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Rowing");
+		new Lwjgl3Application(new MyRowing(), config);
+	}
+}
