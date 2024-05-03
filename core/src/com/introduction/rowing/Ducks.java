3c49f680e63214f01d5579122d5357b69ca5b04e
gdelco <135312403+gdelco@users.noreply.github.com>
2024-05-03 09:49:28 +0200

    Delete core directory

--- core/src/com/introduction/rowing/Ducks.java
+++
@@ -1,9 +0,0 @@
-package com.introduction.rowing;
-
-import com.badlogic.gdx.graphics.Texture;
-
-public class Ducks extends Entity{
-    public Ducks(Position position, int width, int height, Texture image) {
-        super(position, width, height, image);
-    }
-}
--- core/src/com/introduction/rowing/Gees.java
+++
@@ -1,9 +0,0 @@
-package com.introduction.rowing;
-
-import com.badlogic.gdx.graphics.Texture;
-
-public class Gees extends Entity{
-    public Gees(Position position, int width, int height, Texture image) {
-        super(position, width, height, image);
-    }
-}
