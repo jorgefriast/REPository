af54bbd2f7ca155c76a95e18db3d99fe98d50d8b
SenecalJF <jeanjeansenecal@gmail.com>
2024-05-18 17:28:06 +0200

    feat: dragons head for mini game

---
+++ core/src/com/introduction/rowing/DragonHead.java
@@ -0,0 +1,30 @@
+package com.introduction.rowing;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class DragonHead extends Entity{
+    GameInputProcessor inputProcessor;
+    public DragonHead(Position position, int width, int height, Texture image, GameInputProcessor inputProcessor) {
+        super(position, width, height, image);
+        this.inputProcessor = inputProcessor;
+    }
+
+    public void updateKeys(float delta) {
+        if (inputProcessor.moving) {
+            switch (inputProcessor.direction) {
+                case 0:
+                    this.position.setY((int) (this.position.getY() + (150 * delta)));
+                    break;
+                case 1:
+                    this.position.setX((int) (this.position.getX() - (150 * delta)));
+                    break;
+                case 2:
+                    this.position.setY((int) (this.position.getY() - (150 * delta)));
+                    break;
+                case 3:
+                    this.position.setX((int) (this.position.getX() + (150 * delta)));
+                    break;
+            }
+        }
+    }
+}
