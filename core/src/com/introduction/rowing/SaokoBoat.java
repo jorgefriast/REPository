f04bae4c55ba37d5aa8f2ba9328c4b5add94db45
Antonio Mancera Gamez <amanceragamez@gmail.com>
2024-05-31 12:11:23 +0200

    Finished with saoko

--- core/src/com/introduction/rowing/SaokoBoat.java
+++ core/src/com/introduction/rowing/SaokoBoat.java
@@ -1,12 +1,10 @@
 package com.introduction.rowing;
 
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Texture;
 
 public class SaokoBoat extends Boat {
-    private Texture texture;
+    private static Texture texture = new Texture("boats/saoko.png");
     public SaokoBoat(int id, Position position, boolean isPlayer, GameInputProcessor inputProcessor) {
-        super(id, position, new Texture("boats/saoko.png"), isPlayer, 5, 3, 1, 2, 3, 1, inputProcessor);
-        this.texture = new Texture("boats/saoko.png");
+        super(id, position, SaokoBoat.texture, isPlayer, 5, 3, 1, 2, 3, 1, inputProcessor);
     }
 }
